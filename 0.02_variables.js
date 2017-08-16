a6fc44da3cb8956677fa5747e7c14b2e7c96933e
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-08-16 14:09:21 -0400

    Added Variables

---
+++ 0.02_variables.js
@@ -0,0 +1,53 @@
+// Variables
+
+var name;
+
+name = "Madyson";
+
+console.log(typeof(name));
+console.log(name);
+
+var lastName = "Ward";
+
+console.log(name.length);
+console.log(name.toUpperCase());
+
+//Challenge: create three variables: 'firstName' and 'laseName' and concat them together in a third 
+//variable called 'fullName' and print fullName to the console
+
+var firstName = "Tom";
+var lastName = "McClellan";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var myNumber = 0;
+myNumber += 10;
+console.log(myNumber);
+myNumber -= 1;
+console.log(myNumber);
+myNumber *= 9;
+console.log(myNumber);
+myNumber /= 7;
+console.log(myNumber);
+
+var currentAge = 29;
+var maxAge = 100;
+var yearsLeft = maxAge - currentAge;
+var daysLeft = yearsLeft * 365;
+var cupsPerDay = 5;
+var cupsToDrink = cupsPerDay * daysLeft;
+
+console.log("You will need" + " " + cupsToDrink + " " + "cups of tea to last you until the ripe old age of 100.");
+
+
+
+
+// Variable data types
+
+
+
+// Variable scopes
+
+
+
+// 

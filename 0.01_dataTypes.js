fa5edb0c97aa09b5b8dc8a31bfa71392b8455228
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-08-16 13:12:22 -0400

    Added Data Types

---
+++ 0.01_dataTypes.js
@@ -0,0 +1,55 @@
+// DataTypes
+
+// Strings
+
+"What a wonderful world";
+'Hello you guys';
+
+"It's a wonderful day";
+console.log('Thomas said "Grass is green"'.toUpperCase());
+
+console.log('Couldn\'t get things any better'.length);
+
+//Challenge: create a string that has your name and use the to lowercase function to make sure each character is lower case.
+
+console.log("Hello, my name is Thomas James McClellan.  Nice to meet you.".toLowerCase());
+
+console.log('jay'[0].toUpperCase() + 'jay'[1] + 'jay'[2]);
+
+//Challenge: uppercase the m in Home and put the string back together using string concatenation.
+
+console.log('Home'[0] + 'Home'[1] + 'Home'[2].toUpperCase() + 'Home'[3]);
+
+console.log(typeof("Amber"));
+
+// Numbers
+
+console.log(typeof(2));
+
+console.log(2 + 5);
+console.log(2 - 5);
+console.log(2 / 5);
+console.log(2 * 5);
+console.log(10 % 3);
+
+
+//Booleans
+
+console.log(true);
+/* 
+	Conditional Operators:
+
+	<	less than
+	>	greater than
+	<=	less than or eqaul to
+	>=	greater than or equal to
+	==	equal to
+	===	strickly equal to 
+	!	not
+	!=	not equal to
+*/
+
+console.log(4 == 5);
+console.log("Test".length > "ego".length);
+console.log(1 == "1");
+console.log(1 === "1");

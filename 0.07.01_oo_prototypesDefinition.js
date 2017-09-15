53b9801690854b049a2e2d4dcb031b4413b9db04
Thomas McClellan <thomasmcclellan87@gmail.com>
2017-09-15 11:32:40 -0400

    Added oo

---
+++ 0.07.01_oo_prototypesDefinition.js
@@ -0,0 +1,10 @@
+//Every single 'object' is built by a constructor function
+//There are only two languages that deserve the monicer 'object oriented': JS and Liluah
+
+//Each time a constructor is called, a new object is created
+
+//A constructor makes an object 'based on' its own prototype => 'based on' implies false info about JS
+//It implies that we take a prototype and stamp a copy out of it and once the two have been separated, there is no relation between the two
+
+//More accurately: 
+//a constructor makes an object 'linked to' its own prototype
---
+++ 0.07.02_oo_prototypes.js
@@ -0,0 +1,44 @@
+//Object-Oriented Prototypes
+
+//**Before everything, there is a built-in object prototype before any function is called**
+
+//Primary function is created foo() and links with an object with a link called .prototype 
+//The object also links with the function called .constructor
+	//.constructor doesn't mean 'was constructed by' => it is an arbitrary name
+function foo(){
+	this.me = who;
+}
+//We arbitrarily add names to these arbitrarily named objects
+//We call it .identify (directly on the object)
+	//Doesn't matter that .identify is a method => it is just a property at the moment
+Foo.prototype.identify = function(){ 
+	return 'I am' + this.me;
+};
+
+//When we call a new function, four things happens:
+//1) A new object is created
+//2) Oject gets linked to .identify
+//3) The object gets set to 'this' => gets .me property
+//4) Returns 'this' and gets assigned a1
+var a1 = new Foo('a1');
+//Same as a1
+var a2 = new Foo('a2');
+
+//.speak property gets added to a2
+a2.speak = function(){
+	alert('Hello, ' + this.identify() + '.');
+};
+
+//There is no .constructor property on a1
+	//When it doesn't have .constructor, it goes up the prototype chain => [[P]]
+//Links up with .identify, and, thus, to Foo()
+a1.constructor === Foo;
+a1.constructor === a2.constructor;
+//There is no Dunder proto on a1
+	//Likewise, it moves up the [[P]]s to find one
+//There is a Dunder proto in the built-in object prototype(line3)
+a1.__proto__ === Foo.prototype;
+a1.__proto__ == a2.__proto__;
+
+//The links between all of these objects are called prototype links => [[Prototype]] => [[P]] for short
+//__proto__ => Dunder proto
